38dde1a81359414146ace684e71523bbdd724cd1
bertt <btemme@hotmai.com>
2017-05-01 17:55:41 +0200

    getting the test running from docker

--- sta_test_runner.sh
+++ sta_test_runner.sh
@@ -1,20 +1,8 @@
 echo welcome to sta_test_tunner
 docker pull geodan/gost
 docker pull geodan/gost-db
+docker pull geodan/ets-sta10-1.0
 docker-compose up -d
-sleep 10
-curl -H "Content-Type: application/json" -X POST -d '{ "description": "thing 1", "name": "thing name 1", "properties": { "reference": "first" }, "Locations": [ { "description": "location 1", "name": "location name 1", "location": { "type": "Point", "coordinates": [ -117.05, 51.05 ] }, "encodingType": "application/vnd.geo+json" } ], "Datastreams": [ { "unitOfMeasurement": { "name": "Lumen", "symbol": "lm", "definition": "http://www.qudt.org/qudt/owl/1.0.0/unit/Instances.html/Lumen" }, "description": "datastream 1", "name": "datastream name 1", "observationType": "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement", "ObservedProperty": { "name": "Luminous Flux", "definition": "http://www.qudt.org/qudt/owl/1.0.0/quantity/Instances.html/LuminousFlux", "description": "observedProperty 1" }, "Sensor": { "description": "sensor 1", "name": "sensor name 1", "encodingType": "application/pdf", "metadata": "Light flux sensor" }, "Observations":[ { "phenomenonTime": "2015-03-03T00:00:00Z", "result": 3 }, { "phenomenonTime": "2015-03-04T00:00:00Z", "result": 4 } ] }, { "unitOfMeasurement": { "name": "Centigrade", "symbol": "C", "definition": "http://www.qudt.org/qudt/owl/1.0.0/unit/Instances.html/Lumen" }, "description": "datastream 2", "name": "datastream name 2", "observationType": "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement", "ObservedProperty": { "name": "Tempretaure", "definition": "http://www.qudt.org/qudt/owl/1.0.0/quantity/Instances.html/Tempreture", "description": "observedProperty 2" }, "Sensor": { "description": "sensor 2", "name": "sensor name 2", "encodingType": "application/pdf", "metadata": "Tempreture sensor" }, "Observations":[ { "phenomenonTime": "2015-03-05T00:00:00Z", "result": 5 }, { "phenomenonTime": "2015-03-06T00:00:00Z", "result": 6 } ] } ] }' http://localhost:8080/v1.0/Things
-sta_test_result=$(java -jar ./testsuite/1.0/ets-sta10-1.0-aio.jar ./testsuite/1.0/test-run-props.xml)
-file=${sta_test_result##*: } 
-echo sta_test_runner output: $file
-result=$(sed -n '2p' < $file)
-result=${result#"<"}
-result=${result%">"}
-result="${result//\"}"
-result="${result// /,}" 
-
-echo result: $result
-now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-
-curl -H "Content-Type: application/json" -X POST -d '{"phenomenonTime": "'$now'","result" : "'$result'","Datastream":{"@iot.id":"166"}}' http://gost.geodan.nl/v1.0/Observations
+sleep 15
+docker run --network etssta10runner_default geodan/ets-sta10-1.0
 docker-compose down
