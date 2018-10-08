9a174f4311831a530be4fce782d66e3b0c0f3915
bert <btemme@hotmail.com>
2018-10-08 16:01:09 +0200

    network rename

--- sta_test_runner.sh
+++ sta_test_runner.sh
@@ -1,8 +1,8 @@
 echo welcome to sta_test_tunner 1.0
-# docker pull geodan/gost
+docker pull geodan/gost
 docker pull geodan/gost-db
 docker pull geodan/ets-sta10-1.0
 docker-compose up -d
 sleep 15
-docker run --network etssta10runner_default geodan/ets-sta10-1.0
+docker run --network ets-sta10-runner_default geodan/ets-sta10-1.0
 docker-compose down -v
